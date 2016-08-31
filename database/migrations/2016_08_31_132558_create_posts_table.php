0638342caa9178db401c8726a84a39690b86c70a
lararojasmr <destroyer=87>
2016-08-31 10:05:29 -0400

    Se configura el entorno para logearse con distintas plataformas, faltan crear modelos de posts y configurar la publicacion, e interfaces.

---
+++ database/migrations/2016_08_31_132558_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('title');
+            $table->string('img_url');
+            $table->longText('description');
+            $table->timestamps('created_at');
+            $table->integer('owner')->unsigned();
+
+            $table->foreign('owner')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
